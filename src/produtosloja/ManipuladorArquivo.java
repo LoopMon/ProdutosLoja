1673440499dc4079650110d98eafe063a9df39c6
João Lucas <introducaoteste@gmail.com>
2023-09-25 15:09:46 -0300

    Third commit

--- src/produtosloja/ManipuladorArquivo.java
+++ src/produtosloja/ManipuladorArquivo.java
@@ -1,11 +1,8 @@
 package produtosloja;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class ManipuladorArquivo {
     
